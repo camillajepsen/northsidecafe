5419f5fa16db4f46b4a4183d98c22f196cd36301
camillajepsen <camillaruger@gmail.com>
2024-09-25 12:27:35 +1000

    Update config.php

--- adminSide/config.php
+++ adminSide/config.php
@@ -1,18 +1,16 @@
-<?php
 // Define database connection constants
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'Cafe');
+define('DB_HOST', 'your-aws-endpoint');  
+define('DB_USER', 'admin');  
+define('DB_PASS', 'Northsidecafekent2024'); 
+define('DB_NAME', 'cafe');  
 
 // Create connection
 $link = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
 // Check connection
-if ($link->connect_error) { // If connection fails
-    die('Connection failed: ' . $link->connect_error); // Kill the script and display an error message
+if ($link->connect_error) {
+    die('Connection failed: ' . $link->connect_error);
 }
 
-// Optional: Set the character set to utf8 (common practice)
+// Optional: Set the character set to utf8
 $link->set_charset("utf8");
-
